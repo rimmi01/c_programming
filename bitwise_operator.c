b6c2e1e660802ab874c65df73b70993997775a3f
rimmi <goyalrimmi12@gmail.com>
2024-01-31 12:52:57 +0530

    operators, and operator presedence

---
+++ bitwise_operator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int x = 5;
+    // printf("%p", &x); // format specifier for address is %p, the address will be 64 bit means 8 bytes long
+    // every byte have a specific address in memory
+    // printf("%d"); // if we don't give any variable, then it will give some random value
+
+
+    // printf("%d", 2+3*5);
+
+
+    // ********function call******//
+    // printf("%d", sizeof(int));  // sizeof function is the number of bytes the memory is containing for ()
+    // printf("%d", sizeof(long int));  // size
+    float y;
+    // printf("%d", sizeof(y));
+    // printf("%d", sizeof(double));
+    // printf("%d", sizeof(void));  // it's(void) using as a written type in memeory
+    printf("%d", 5<6);  //output will be 1, if the codition is true and for false it's 0
+    return 0;
+}
---
+++ operators.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main()
+{
+    // int i = -3;
+    // int k = i % 2;  // modulus operator which gives remainder
+    // printf("%d\n", k);
+    // int i = 3;
+    // int l = i / -2;
+    // int k = i % -2;
+    // printf("%d%d\n", l, k);  // don't understood
+    // printf("%f", 12/9); // while directly using the number, it will give the integer value only
+    // printf("%f", 12/9.0); // at least, one of the number needs to be float
+    // int x = 5.3 % 2; // remainder is always a integer value
+    // printf("Value of x is %d", x);
+
+    // int a = 10;
+    // double b = 5.6;
+    // int c;
+    // c = a + b;
+    // printf("%d", c);  // if, we use %d, then it will take only the integer part of the variables
+
+    // int a = 234365;
+    // printf("%f", a); // answer is zero as we use %f and the type of variable is int
+
+    // float f = 3.456;
+    // printf("%d", f==3.456f); // f at the last is like
+
+    // *********unary operator**********//
+    // int i = 9;
+    // int x = i++ + ++i + i++;      // post fix and prefix concept
+    // printf("%d", x);
+
+    // int i = 9;
+    // int x = 4 * i++ /7 ;     //
+    // printf("%d", x);
+
+    // int x = 3, y, z;
+    // y = x++;
+    // z = ++x;
+    // printf("\n%d,%d,%d", x, y, z);
+
+    // int a = 1, b = 1, c;
+    // c = a++ + b;
+    // printf("\n%d,%d,%d", a, b, c);  // prefix works first
+
+    // int a = 1, b = 2, c;
+    // c = a > b;
+    // printf("\n%d", c); // output is 0, as this is false
+
+    // int a = 10, b = 5, c=5;
+    // int d = b + c == a;
+    // printf("\n%d", d);
+
+    //***********Logical operators************//
+    // int a = -8, b = 0;
+    // int x = a && b == 1;
+    // printf("%d", x);
+
+    // int a = -8, b = 0;
+    // int x = a += b == 1;
+    // printf("%d", x);
+
+    // int x = 1, y= 0, z = 5;
+    // int a = x && y && z ++;
+    // printf("%d", z);    //why 5?
+
+    return 0;
+}
+
+// ********operator types*********//
+// Arithmetic operator
+// unary operator
+// relational operator
+// logical operator
+// Assignment operator
+// Conditional operator
+// Bitwise operator
+// Special operator
